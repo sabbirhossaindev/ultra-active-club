40e34f6dd9aab169eef0d9500e14df9df4ca860a
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-29 12:01:01 +0600

    card added

---
+++ src/components/Exercise/Exercise.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
+import Gim from '../Gim/Gim';
+import './Exercise.css';
+
+const Exercise = () => {
+    const [gims, setGims] = useState([])
+
+    useEffect(() => {
+        fetch('gim.json')
+            .then(rse => rse.json())
+            .then(data =>setGims(data))
+    }, [])
+
+    return (
+        <div className="exercise-container">
+            <div className="gim-container">
+                {
+                    gims.map(gim =><Gim></Gim>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Exercise;
